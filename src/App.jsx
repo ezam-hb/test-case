524e4df8f210ec68593b82810ab0a5b6c2b1186e
ezam-hb <ezam.habibulbashar@gmail.com>
2024-01-13 13:02:06 +0600

    test

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className={`flex h-screen items-center justify-center`}>
+      <h1 className="text-3xl font-bold">Hi! I am working Properly...</h1>
+    </div>
+  );
+};
+
+export default App;
